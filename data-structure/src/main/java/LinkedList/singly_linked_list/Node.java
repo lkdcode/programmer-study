675859217d816475ab6b1b78df302dc62c680b30
lkdcode <dlrlejr1@naver.com>
2023-09-17 23:03:35 +0900

    [feat] singlyLinkedList : Add Node

---
+++ data-structure/src/main/java/LinkedList/singly_linked_list/Node.java
@@ -0,0 +1,16 @@
+package LinkedList.singly_linked_list;
+
+public class Node<E> {
+    public E data;
+    public Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+}
