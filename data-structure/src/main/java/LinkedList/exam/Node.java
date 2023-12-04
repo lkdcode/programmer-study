072dbd9ba755014ec20e8d0c4cea7f82554e5cac
lkdcode <dlrlejr1@naver.com>
2023-12-04 13:21:58 +0900

    initial commit

---
+++ data-structure/src/main/java/LinkedList/exam/Node.java
@@ -0,0 +1,17 @@
+package LinkedList.exam;
+
+public class Node<E> {
+    public E data;
+    public Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+}
