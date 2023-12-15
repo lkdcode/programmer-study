1c0e53b127cab17e07371a6340c1cfdb93613a13
lkdcode <dlrlejr1@naver.com>
2023-12-15 23:51:52 +0900

    spring-security

---
+++ spring-boot-practice/security/src/main/java/com/programmersstudy/springbootpractice/SpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package src.main.java.com.programmersstudy.springbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeApplication.class, args);
+	}
+
+}
