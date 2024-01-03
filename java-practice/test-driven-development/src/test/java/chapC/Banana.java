f9e1683a6505c08b72730124c6088e9235e02fa4
lkdcode <dlrlejr1@naver.com>
2024-01-04 00:38:59 +0900

    TDD [chapC]

---
+++ java-practice/test-driven-development/src/test/java/chapC/Banana.java
@@ -0,0 +1,13 @@
+package chapC;
+
+class Banana implements Fruit {
+    private final String name;
+
+    public Banana(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
