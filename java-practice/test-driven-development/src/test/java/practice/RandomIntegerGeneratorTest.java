ceb9c613af227be995320929ddf3f1e70c5ef699
lkdcode <dlrlejr1@naver.com>
2024-01-06 23:57:12 +0900

    RandomIntegerGenerator : TDD

---
+++ java-practice/test-driven-development/src/test/java/practice/RandomIntegerGeneratorTest.java
@@ -0,0 +1,17 @@
+package practice;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomIntegerGeneratorTest {
+    @RepeatedTest(5_000)
+    void randomNumberMaker() {
+        final int number = RandomIntegerGenerator.getNumber();
+
+        assertThat(number)
+                .isGreaterThanOrEqualTo(0)
+                .isLessThanOrEqualTo(9);
+    }
+
+}
