c90f5782005aeccb63040535fbd44fe60286346b
lkdcode <dlrlejr1@naver.com>
2024-01-06 23:57:12 +0900

    RandomIntegerGenerator : TDD

---
+++ java-practice/test-driven-development/src/main/java/practice/RandomIntegerGenerator.java
@@ -0,0 +1,12 @@
+package practice;
+
+import java.security.SecureRandom;
+
+public class RandomIntegerGenerator {
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
+
+    public static int getNumber() {
+        return SECURE_RANDOM.nextInt(10);
+    }
+
+}
