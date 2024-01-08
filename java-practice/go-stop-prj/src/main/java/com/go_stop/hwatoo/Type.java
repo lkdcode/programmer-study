999933112f2063f185569eff4f37da02836bc824
lkdcode <dlrlejr1@naver.com>
2024-01-08 11:55:30 +0900

    Feat : domain card

---
+++ java-practice/go-stop-prj/src/main/java/com/go_stop/hwatoo/Type.java
@@ -0,0 +1,17 @@
+package com.go_stop.hwatoo;
+
+public enum Type {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+    Joker
+}
