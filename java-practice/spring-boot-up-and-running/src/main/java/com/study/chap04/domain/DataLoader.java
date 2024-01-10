1a64d30f6ad8e2e93747a3887d874813834f0f04
lkdcode <dlrlejr1@naver.com>
2024-01-10 14:28:46 +0900

    spring-boot-up-and-running [chap04]

---
+++ java-practice/spring-boot-up-and-running/src/main/java/com/study/chap04/domain/DataLoader.java
@@ -0,0 +1,26 @@
+package com.study.chap04.domain;
+
+import com.study.chap04.repository.CoffeeRepository;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+public class DataLoader {
+    private final CoffeeRepository coffeeRepository;
+
+    public DataLoader(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+    }
+
+    @PostConstruct
+    private void loadData() {
+        this.coffeeRepository.saveAll(List.of(
+                new Coffee("Cafe Cereza"),
+                new Coffee("Cafe Ganador"),
+                new Coffee("Cafe Lareno"),
+                new Coffee("Cafe Tres Pontas")
+        ));
+    }
+}
