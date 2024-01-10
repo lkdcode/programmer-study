c32890621f890cff594bce689bd8d8136b610830
lkdcode <dlrlejr1@naver.com>
2024-01-10 13:56:46 +0900

    spring-boot-up-and-running [chap03]

---
+++ java-practice/spring-boot-up-and-running/src/main/java/com/study/chap03/api/RestApiDemoController.java
@@ -0,0 +1,67 @@
+package com.study.chap03.api;
+
+import com.study.chap03.domain.Coffee;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/coffees")
+public class RestApiDemoController {
+    private List<Coffee> coffees = new ArrayList<>();
+
+    public RestApiDemoController() {
+        this.coffees.addAll(List.of(
+                new Coffee("Cafe Cereza"),
+                new Coffee("Cafe Ganador"),
+                new Coffee("Cafe Lareno"),
+                new Coffee("Cafe Tres Pontas")
+        ));
+    }
+
+
+    @GetMapping
+// @RequestMapping(value = "/coffees", method = RequestMethod.GET)
+    Iterable<Coffee> getCoffees() {
+        return this.coffees;
+    }
+
+    @GetMapping("/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id) {
+        return this.coffees.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst();
+    }
+
+    @PostMapping
+    Coffee postCoffee(@RequestBody Coffee coffee) {
+        this.coffees.add(coffee);
+        return coffee;
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+        int coffeeIndex = -1;
+
+        for (Coffee c : coffees) {
+            if (c.getId().equals(id)) {
+                coffeeIndex = coffees.indexOf(c);
+                coffees.set(coffeeIndex, coffee);
+            }
+        }
+
+        return (coffeeIndex == -1) ?
+                new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
+                new ResponseEntity<>(coffee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteCoffee(@PathVariable String id) {
+        this.coffees.removeIf(c -> c.getId().equals(id));
+    }
+}
