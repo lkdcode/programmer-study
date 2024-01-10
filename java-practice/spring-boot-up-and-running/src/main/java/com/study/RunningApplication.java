5b9bb62ea83084d2dd6209fb10a9df59692cae43
lkdcode <dlrlejr1@naver.com>
2024-01-10 21:26:20 +0900

    spring-boot-up-and-running [chap05]

--- java-practice/spring-boot-up-and-running/src/main/java/com/study/RunningApplication.java
+++ java-practice/spring-boot-up-and-running/src/main/java/com/study/RunningApplication.java
@@ -1,11 +1,22 @@
 package com.study;
 
+import com.study.chap05.domain.Droid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class RunningApplication {
     public static void main(String[] args) {
         SpringApplication.run(RunningApplication.class, args);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    Droid createDroid() {
+        return new Droid();
     }
 }
