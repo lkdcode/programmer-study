81d3582063482f6d7d2cc9fda54139b038b18958
lkdcode <dlrlejr1@naver.com>
2024-05-05 23:08:46 +0900

    Feat(.security): security-6 filter list

---
+++ java-practice/spring-security-6-multi-filter/src/main/java/org/multifilter/security/filter/AppleSecurityFilter.java
@@ -0,0 +1,37 @@
+package org.multifilter.security.filter;
+
+
+import lombok.RequiredArgsConstructor;
+import org.multifilter.security.BaseSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AppleSecurityFilter {
+    private static final String APPLE_API_PREFIX = "/apple"; // /apple
+    private static final String API = APPLE_API_PREFIX + "/**"; // /apple/**/get
+    private static final String[] ALLOW_LIST = {
+            APPLE_API_PREFIX + "/get",
+    };
+    private static final String[] AUTHENTICATED = {
+            APPLE_API_PREFIX + "/secret",
+    };
+
+    private final BaseSecurity baseSecurity;
+
+
+    public SecurityFilterChain doFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher(API)
+                .with(baseSecurity, BaseSecurity::active)
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(ALLOW_LIST).permitAll()
+                        .requestMatchers(AUTHENTICATED).authenticated()
+                        .anyRequest().authenticated())
+
+                .build();
+    }
+}
