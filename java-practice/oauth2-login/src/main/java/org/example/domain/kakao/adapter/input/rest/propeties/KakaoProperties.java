d078c2b120a2d3b9d27d151f2405d925bcf5ff57
lkdcode <dlrlejr1@naver.com>
2024-05-25 20:27:16 +0900

    Refactor(.oauth2): 일괄 주석

--- java-practice/oauth2-login/src/main/java/org/example/domain/kakao/adapter/input/rest/propeties/KakaoProperties.java
+++ java-practice/oauth2-login/src/main/java/org/example/domain/kakao/adapter/input/rest/propeties/KakaoProperties.java
@@ -1,25 +1,23 @@
-package org.example.domain.kakao.adapter.input.rest.propeties;
-
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Getter
-@Component
-public class KakaoProperties {
-    @Value("${kakao.oauth_uri}")
-    private String oauthUri;
-    @Value("${kakao.user_uri}")
-    private String userUri;
-
-
-
-    @Value("${kakao.token.grant_type}")
-    private String grantType;
-    @Value("${kakao.token.client_id}")
-    private String clientId;
-    @Value("${kakao.token.client_secret}")
-    private String clientSecret;
-    @Value("${kakao.uri.redirect_uri}")
-    private String redirectUri;
-}
+//package org.example.domain.kakao.adapter.input.rest.propeties;
+//
+//import lombok.Getter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//@Getter
+//@Component
+//public class KakaoProperties {
+//    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+//    private String oauthUri;
+//    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
+//    private String userUri;
+//
+//    @Value("${spring.security.oauth2.client.registration.kakao.token.grant_type}")
+//    private String grantType;
+//    @Value("${spring.security.oauth2.client.registration.kakao.token.client_id}")
+//    private String clientId;
+//    @Value("${spring.security.oauth2.client.registration.kakao.token.client_secret}")
+//    private String clientSecret;
+//    @Value("${spring.security.oauth2.client.registration.kakao.uri.redirect_uri}")
+//    private String redirectUri;
+//}
--- java-practice/oauth2-login/src/main/java/org/example/domain/naver/adapter/input/rest/NaverApi.java
+++ java-practice/oauth2-login/src/main/java/org/example/domain/naver/adapter/input/rest/NaverApi.java
@@ -1,59 +1,59 @@
-package org.example.domain.naver.adapter.input.rest;
-
-import lombok.RequiredArgsConstructor;
-import org.example.domain.naver.adapter.input.rest.dto.NaverInfoResponseDTO;
-import org.example.domain.naver.adapter.input.rest.dto.NaverResponseDTO;
-import org.example.domain.naver.adapter.input.rest.propeties.NaverProperties;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
-
-import java.util.Objects;
-
-@RestController
-@RequestMapping("/naver")
-@RequiredArgsConstructor
-public class NaverApi {
-    private final NaverProperties naverProperties;
-
-    @GetMapping("/callback")
-    public void getCallback(
-            @RequestParam(name = "code") final String code
-    ) {
-        getToken(code);
-    }
-
-    private void getToken(final String code) {
-        final var restClient = RestClient.builder()
-                .baseUrl(naverProperties.getOauthUri())
-                .build();
-
-        final var body = restClient.get()
-                .uri(uri -> uri
-                        .queryParam("grant_type", naverProperties.getGrantType())
-                        .queryParam("client_id", naverProperties.getClientId())
-                        .queryParam("client_secret", naverProperties.getClientSecret())
-                        .queryParam("code", code)
-                        .queryParam("state", naverProperties.getState())
-                        .build())
-                .retrieve()
-                .body(NaverResponseDTO.class);
-
-        getInfo(Objects.requireNonNull(body).access_token());
-    }
-
-    private void getInfo(final String token) {
-        final var restClient = RestClient.builder()
-                .baseUrl(naverProperties.getUserInfoUri())
-                .build();
-
-        var body = restClient.get()
-                .header("Authorization", "Bearer " + token)
-                .retrieve()
-                .body(NaverInfoResponseDTO.class);
-
-        System.out.println(body);
-    }
-}
+//package org.example.domain.naver.adapter.input.rest;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.example.domain.naver.adapter.input.rest.dto.NaverInfoResponseDTO;
+//import org.example.domain.naver.adapter.input.rest.dto.NaverResponseDTO;
+//import org.example.domain.naver.adapter.input.rest.propeties.NaverProperties;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.client.RestClient;
+//
+//import java.util.Objects;
+//
+//@RestController
+//@RequestMapping("/naver")
+//@RequiredArgsConstructor
+//public class NaverApi {
+//    private final NaverProperties naverProperties;
+//
+//    @GetMapping("/callback")
+//    public void getCallback(
+//            @RequestParam(name = "code") final String code
+//    ) {
+//        getToken(code);
+//    }
+//
+//    private void getToken(final String code) {
+//        final var restClient = RestClient.builder()
+//                .baseUrl(naverProperties.getOauthUri())
+//                .build();
+//
+//        final var body = restClient.get()
+//                .uri(uri -> uri
+//                        .queryParam("grant_type", naverProperties.getGrantType())
+//                        .queryParam("client_id", naverProperties.getClientId())
+//                        .queryParam("client_secret", naverProperties.getClientSecret())
+//                        .queryParam("code", code)
+//                        .queryParam("state", naverProperties.getState())
+//                        .build())
+//                .retrieve()
+//                .body(NaverResponseDTO.class);
+//
+//        getInfo(Objects.requireNonNull(body).access_token());
+//    }
+//
+//    private void getInfo(final String token) {
+//        final var restClient = RestClient.builder()
+//                .baseUrl(naverProperties.getUserInfoUri())
+//                .build();
+//
+//        var body = restClient.get()
+//                .header("Authorization", "Bearer " + token)
+//                .retrieve()
+//                .body(NaverInfoResponseDTO.class);
+//
+//        System.out.println(body);
+//    }
+//}
