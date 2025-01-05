a491866fffbc20437aa69eea3e852f2c0ff39d8e
lkdcode <dlrlejr1@naver.com>
2025-01-05 17:35:36 +0900

    Feat(:Real-My-Sql)

---
+++ java-practice/readl-my-sql-first/src/main/java/book/ex/Application.java
@@ -0,0 +1,8 @@
+package book.ex;
+
+public class Application {
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        System.out.println(apple.getName());
+    }
+}
