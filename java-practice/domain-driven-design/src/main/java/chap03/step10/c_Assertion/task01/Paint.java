61324b4d632cee6ed09da46959f56a1ca4f474fc
lkdcode <dlrlejr1@naver.com>
2025-01-11 21:19:22 +0900

    Feat(:DDD) 더 심층적인 통찰을 향한 리팩터링

---
+++ java-practice/domain-driven-design/src/main/java/chap03/step10/c_Assertion/task01/Paint.java
@@ -0,0 +1,13 @@
+package chap03.step10.c_Assertion.task01;
+
+class Paint {
+    double volume;
+    PigmentColor pigmentColor;
+
+    public Paint(double volume, PigmentColor pigmentColor) {
+        this.volume = volume;
+        this.pigmentColor = pigmentColor;
+    }
+
+
+}
