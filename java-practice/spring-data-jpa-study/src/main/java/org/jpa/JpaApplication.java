40872751c1a4ce769da7cff8e0b3ae2055aa3ffc
lkdcode <dlrlejr1@naver.com>
2025-01-31 22:20:24 +0900

    Chore(:yml) jpa&schedule config

--- java-practice/spring-data-jpa-study/src/main/java/org/jpa/JpaApplication.java
+++ java-practice/spring-data-jpa-study/src/main/java/org/jpa/JpaApplication.java
@@ -2,8 +2,13 @@ package org.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "org/jpa/chap02",
+    "org/jpa/chap03",
+})
 public class JpaApplication {
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
