de130751426a549c9b83d378cf8691675e46028b
lkdcode <dlrlejr1@naver.com>
2025-04-01 22:31:52 +0900

    feat(:react) study

--- presentation/moku-demo/src/App.jsx
+++ presentation/moku-demo/src/App.jsx
@@ -1,14 +1,59 @@
 import "./App.css";
 
-import Header from "./components/Header";
 import Button from "./components/Button";
 import Board from "./components/Board";
+import axios from "axios";
 
 function App() {
+  const baseUrl = import.meta.env.DEV_BASE_URL;
+
+  const onGet = () => {
+    axios
+      .get(`${baseUrl}/api`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
+
+  const onPost = () => {
+    axios
+      .post(`${baseUrl}/api`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
+
+  const onDelete = () => {
+    axios
+      .delete(`${baseUrl}/api`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
+
+  const onPut = () => {
+    axios
+      .put(`${baseUrl}/api`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Button />
+      <Button title={"시작하기"} onClickFun={onGet} />
       <Board size={15} />
     </div>
   );
