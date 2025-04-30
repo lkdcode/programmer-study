c338306796d8d1939415ad1d03559faf6e8ae459
lkdcode <dlrlejr1@naver.com>
2025-05-01 00:22:48 +0900

    Feat(:ecommerce) init

---
+++ wanted-preonboarding-challenge-backend-31-main/buildSrc/src/main/java/TestSet.java
@@ -0,0 +1,7 @@
+public class TestSet {
+    public static final String TEST_CONTAINER = "org.springframework.boot:spring-boot-testcontainers";
+    public static final String TEST_CONTAINER_PSQL = "org.testcontainers:postgresql";
+
+    public static final String JUNIT = "org.testcontainers:junit-jupiter";
+    public static final String JUNIT_LAUNCHER = "org.junit.platform:junit-platform-launcher";
+}
