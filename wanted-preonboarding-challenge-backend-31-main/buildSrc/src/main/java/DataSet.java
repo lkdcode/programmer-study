83527d2fe38b93aa380e15343501f6ba73c2fcc1
lkdcode <dlrlejr1@naver.com>
2025-05-01 00:22:48 +0900

    Feat(:ecommerce) init

---
+++ wanted-preonboarding-challenge-backend-31-main/buildSrc/src/main/java/DataSet.java
@@ -0,0 +1,6 @@
+public class DataSet {
+    public static final String FLYWAY_CORE = "org.flywaydb:flyway-core";
+    public static final String FLYWAY_PSQL = "org.flywaydb:flyway-database-postgresql";
+
+    public static final String PSQL_CONNECTOR = "org.postgresql:postgresql";
+}
---
+++ wanted-preonboarding-challenge-backend-31-main/buildSrc/src/main/java/SpringBootStarter.java
@@ -0,0 +1,8 @@
+public class SpringBootStarter {
+    public static final String WEB = "org.springframework.boot:spring-boot-starter-web";
+    public static final String DATA_JPA = "org.springframework.boot:spring-boot-starter-data-jpa";
+    public static final String VALIDATION = "org.springframework.boot:spring-boot-starter-validation";
+    public static final String TEST = "org.springframework.boot:spring-boot-starter-test";
+    public static final String LOMBOK_COMPILE_ONLY = "org.projectlombok:lombok";
+    public static final String LOMBOK_ANNOTATION_PROCESSOR = "org.projectlombok:lombok";
+}
