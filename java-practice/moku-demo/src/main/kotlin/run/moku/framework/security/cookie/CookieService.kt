5ee50214d9c99f20d3975f5a39be83879e893595
lkdcode <dlrlejr1@naver.com>
2025-04-30 19:59:32 +0900

    Feat(:moku) security init

---
+++ java-practice/moku-demo/src/main/kotlin/run/moku/framework/security/cookie/CookieService.kt
@@ -0,0 +1,14 @@
+package run.moku.framework.security.cookie
+
+import org.springframework.stereotype.Service
+
+@Service
+class CookieService {
+    fun createCookie() {
+
+    }
+
+    fun removeCookie() {
+
+    }
+}
