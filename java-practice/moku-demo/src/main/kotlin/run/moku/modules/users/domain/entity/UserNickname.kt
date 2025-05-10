e634f06b6ac2d37df578e9a9619b2116992a37b3
lkdcode <dlrlejr1@naver.com>
2025-05-11 00:00:06 +0900

    Feat(:user) add domain model

--- java-practice/moku-demo/src/main/kotlin/run/moku/modules/users/domain/entity/UserNickname.kt
+++ java-practice/moku-demo/src/main/kotlin/run/moku/modules/users/domain/entity/UserNickname.kt
@@ -2,19 +2,20 @@ package run.moku.modules.users.domain.entity
 
 @JvmInline
 value class UserNickname private constructor(
-    private val value: String
+    val value: String
 ) {
     init {
-        require(value.isNotEmpty()) { "닉네임은 필수입니다." }
+        require(value.isNotEmpty()) { REQUIRED_MESSAGE }
         require(value.length in MIN_LENGTH..MAX_LENGTH) { LENGTH_VALID_MESSAGE }
     }
 
-    fun asString(): String = value
-
     companion object {
+        const val REQUIRED_MESSAGE = "닉네임은 필수입니다."
+
         const val MIN_LENGTH = 4;
         const val MAX_LENGTH = 20;
-        const val LENGTH_VALID_MESSAGE = "닉네임은 4~20자만 허용됩니다.";
+        const val LENGTH_VALID_MESSAGE = "닉네임은 ${UserLoginId.MIN_LENGTH}~${UserLoginId.MAX_LENGTH}자리만 허용됩니다.";
+
         fun of(value: String): UserNickname = UserNickname(value)
     }
 }
--- java-practice/moku-demo/src/main/kotlin/run/moku/modules/users/domain/value/UserLoginPassword.kt
+++ java-practice/moku-demo/src/main/kotlin/run/moku/modules/users/domain/value/UserLoginPassword.kt
@@ -2,11 +2,15 @@ package run.moku.modules.users.domain.value
 
 @JvmInline
 value class UserLoginPassword private constructor(
-    private val value: String
+    val value: String
 ) {
     init {
-        require(value.isNotEmpty()) { "로그인 비밀번호를 입력해주세요." }
+        require(value.isNotEmpty()) { REQUIRED_MESSAGE }
     }
 
-    fun asString() = value
+    companion object {
+        const val REQUIRED_MESSAGE = "로그인 비밀번호는 필수입니다."
+
+        fun of(value: String): UserLoginPassword = UserLoginPassword(value)
+    }
 }
