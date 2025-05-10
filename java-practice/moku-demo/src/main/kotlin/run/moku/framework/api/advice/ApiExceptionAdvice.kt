814b0cc15e46f647b5fefde5ce02918b61de6f66
lkdcode <dlrlejr1@naver.com>
2025-05-10 21:38:00 +0900

    Feat(:api) add common api response

---
+++ java-practice/moku-demo/src/main/kotlin/run/moku/framework/api/advice/ApiExceptionAdvice.kt
@@ -0,0 +1,149 @@
+package run.moku.framework.api.advice
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.ConstraintViolationException
+import org.springframework.dao.DataAccessException
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpRequestMethodNotSupportedException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.MissingRequestHeaderException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.resource.NoResourceFoundException
+import run.moku.framework.api.exception.ApiException
+import run.moku.framework.api.response.ApiResponse
+import run.moku.framework.api.response.ApiResponseCode
+import run.moku.framework.log.logging
+import java.sql.SQLException
+
+@RestControllerAdvice
+class ApiExceptionAdvice {
+
+    @ExceptionHandler(ApiException::class)
+    fun handleApiException(e: ApiException): ResponseEntity<ApiResponse<String>> {
+        logging().info("ApiResponseCode: ${e.getApiResponseCode()}, Message: ${e.message()} Status: ${e.status()}")
+
+        return ApiResponse.clientError(
+            status = e.status(),
+            message = e.message(),
+            code = e.code()
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ApiResponse<List<*>>> {
+        val response = e.bindingResult.fieldErrors.map {
+            logging().info("Field: ${it.field}, Rejected value: ${it.rejectedValue}, Message: ${it.defaultMessage}")
+
+            mapOf(
+                "Field" to it.field,
+                "Rejected value" to it.rejectedValue,
+                "Message" to it.defaultMessage
+            )
+        }
+
+        return ApiResponse.clientError(
+            apiResponseCode = ApiResponseCode.CLIENT_INVALID_REQUEST,
+            payload = response
+        )
+    }
+
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun handleConstraintViolationException(e: ConstraintViolationException): ResponseEntity<ApiResponse<List<*>>> {
+        val response = e.constraintViolations.map {
+            logging().info("Path: ${it.rootBeanClass}, Property: ${it.propertyPath} Invalid value: ${it.invalidValue}, Message: ${it.message}")
+
+            mapOf(
+                "Invalid value" to it.propertyPath.toString(),
+                "Message" to it.message
+            )
+        }
+
+        return ApiResponse.clientError(
+            apiResponseCode = ApiResponseCode.CLIENT_INVALID_DATA,
+            payload = response,
+        )
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): ResponseEntity<ApiResponse<String>> {
+        logging().info("HttpMessageNotReadableException ${e.message}")
+
+        return ApiResponse.clientError(
+            apiResponseCode = ApiResponseCode.CLIENT_INVALID_BODY,
+            payload = "${e.message}", /* TODO : 삭제? 패키지 경로가 다 나옴. */
+        )
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException::class)
+    fun handleMissingRequestHeaderException(e: MissingRequestHeaderException): ResponseEntity<ApiResponse<String>> {
+        logging().info("${e.body.detail}")
+
+        return ApiResponse.clientError(
+            apiResponseCode = ApiResponseCode.CLIENT_MISSING_HEADER,
+            payload = "${e.body.detail}",
+        )
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    fun handleHttpRequestMethodNotSupportedException(
+        e: HttpRequestMethodNotSupportedException,
+        request: HttpServletRequest,
+    ): ResponseEntity<ApiResponse<String>> {
+        logging().info("Method: ${e.method} End-Point: ${request.requestURI}")
+
+        return ApiResponse.clientError(
+            apiResponseCode = ApiResponseCode.CLIENT_UNSUPPORTED_METHOD,
+            payload = "${e.method} ${request.requestURI} ${e.message}",
+        )
+    }
+
+    @ExceptionHandler(NoResourceFoundException::class)
+    fun handleNoResourceFoundException(
+        e: NoResourceFoundException
+    ): ResponseEntity<ApiResponse<String>> {
+        logging().info("End-Point: ${e.resourcePath} Message: ${e.message}")
+
+        return ApiResponse.clientError(
+            apiResponseCode = ApiResponseCode.CLIENT_UNSUPPORTED_REQUEST,
+            payload = "API: /${e.resourcePath}, Message: ${e.message}",
+        )
+    }
+
+    @ExceptionHandler(SQLException::class)
+    fun handleSQLException(e: SQLException): ResponseEntity<ApiResponse<Unit>> {
+        logging().warn("SQLException!!")
+
+        return ApiResponse.serverError(
+            apiResponseCode = ApiResponseCode.SEVER_SQL_EXCEPTION
+        )
+    }
+
+    @ExceptionHandler(DataAccessException::class)
+    fun handleDataAccessException(e: DataAccessException): ResponseEntity<ApiResponse<Unit>> {
+        logging().warn("DataAccessException Message: ${e.message}")
+
+        return ApiResponse.serverError(
+            apiResponseCode = ApiResponseCode.SEVER_DATABASE_EXCEPTION
+        )
+    }
+
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(e: RuntimeException): ResponseEntity<ApiResponse<Unit>> {
+        logging().error("[Runtime Exception] ${e::class.simpleName}: ${e.message}", e)
+
+        return ApiResponse.serverError(
+            apiResponseCode = ApiResponseCode.SEVER_UNHANDLED_EXCEPTION
+        )
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(e: Exception): ResponseEntity<ApiResponse<Unit>> {
+        logging().error("[Unhandled Exception] ${e::class.simpleName}: ${e.message}", e)
+
+        return ApiResponse.serverError(
+            apiResponseCode = ApiResponseCode.SEVER_CRITICAL_EXCEPTION
+        )
+    }
+}
---
+++ java-practice/moku-demo/src/main/kotlin/run/moku/framework/api/response/ApiResponseCode.kt
@@ -0,0 +1,33 @@
+package run.moku.framework.api.response
+
+import org.springframework.http.HttpStatus
+
+enum class ApiResponseCode(
+    val code: String,
+    val status: HttpStatus,
+    val message: String,
+) {
+
+    OK("S001", HttpStatus.OK, "리소스 요청에 성공했습니다."),
+    CREATED("S002", HttpStatus.CREATED, "리소스 생성에 성공했습니다."),
+    DELETED("S003", HttpStatus.NO_CONTENT, "리소스 삭제 요청에 성공했습니다."),
+
+
+    /* CLIENT */
+    CLIENT_INVALID_REQUEST("CC001", HttpStatus.BAD_REQUEST, "유효성 검증에 실패했습니다."),
+    CLIENT_INVALID_DATA("CC002", HttpStatus.BAD_REQUEST, "유효하지 않은 데이터입니다."),
+    CLIENT_INVALID_BODY("CC003", HttpStatus.BAD_REQUEST, "요청 바디가 잘못되었습니다."),
+    CLIENT_MISSING_HEADER("CC004", HttpStatus.BAD_REQUEST, "필수 헤더가 누락되었습니다."),
+    CLIENT_UNSUPPORTED_METHOD("CC005", HttpStatus.BAD_REQUEST, "지원하지 않는 HTTP 메서드입니다."),
+    CLIENT_UNSUPPORTED_REQUEST("CC006", HttpStatus.BAD_REQUEST, "지원하지 않는 요청입니다."),
+
+    /* USER */
+    DUPLICATE_LOGIN_ID("U001", HttpStatus.CONFLICT, "이미 존재하는 로그인 아이디입니다."),
+    DUPLICATE_NICKNAME("U002", HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
+
+    /* SERVER */
+    SEVER_SQL_EXCEPTION("SE001", HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 SQL 에러가 발생했습니다."),
+    SEVER_DATABASE_EXCEPTION("SE002", HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 에러가 발생했습니다."),
+    SEVER_UNHANDLED_EXCEPTION("SE999", HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 에러가 발생했습니다."),
+    SEVER_CRITICAL_EXCEPTION("SE999", HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 에러가 발생했습니다."),
+}
