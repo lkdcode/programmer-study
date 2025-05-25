155c03711316366f83778f3d023b35b6a67eb0ec
lkdcode <dlrlejr1@naver.com>
2025-05-25 21:49:09 +0900

    Feat(:moku) add adapter
    - command, query, validator

---
+++ java-practice/moku-demo/src/main/kotlin/run/moku/modules/gomoku/adapter/out/infrastructure/repository/MatchConcurrentLinkedQueue.kt
@@ -0,0 +1,22 @@
+package run.moku.modules.gomoku.adapter.out.infrastructure.repository
+
+import org.springframework.stereotype.Service
+import run.moku.modules.gomoku.domain.entity.player.MokuPlayer
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
+@Service
+class MatchConcurrentLinkedQueue {
+
+    fun addQueue(player: MokuPlayer) {
+        QUEUE.add(player)
+    }
+
+    fun popUser(): MokuPlayer = QUEUE.poll()
+
+    fun getSize(): Int = QUEUE.size
+
+    companion object {
+        private val QUEUE: Queue<MokuPlayer> = ConcurrentLinkedQueue()
+    }
+}
---
+++ java-practice/moku-demo/src/main/kotlin/run/moku/modules/gomoku/adapter/out/infrastructure/repository/PlayConcurrentMap.kt
@@ -0,0 +1,21 @@
+package run.moku.modules.gomoku.adapter.out.infrastructure.repository
+
+import org.springframework.stereotype.Service
+import run.moku.modules.gomoku.domain.entity.board.BoardId
+import run.moku.modules.gomoku.domain.model.MokuPlayingModel
+import java.lang.IllegalArgumentException
+import java.util.concurrent.ConcurrentHashMap
+
+@Service
+class PlayConcurrentMap {
+
+    fun save(model: MokuPlayingModel) {
+        LIST[model.boardId] = model
+    }
+
+    fun loadById(boardId: BoardId) = LIST[boardId] ?: throw IllegalArgumentException("!!!!!!!")
+
+    companion object {
+        private val LIST: MutableMap<BoardId, MokuPlayingModel> = ConcurrentHashMap()
+    }
+}
