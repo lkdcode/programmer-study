00095fa234e9af14e3e83bac70d8f53ec86e8259
lkdcode <dlrlejr1@naver.com>
2025-11-10 22:59:17 +0900

    Feat(:webflux) defer

---
+++ api-practice/WebFlux/0-webflux/src/main/kotlin/dev/lkdcode/reactive/operator/Defer.kt
@@ -0,0 +1,63 @@
+package dev.lkdcode.reactive.operator
+
+import reactor.core.publisher.Mono
+import java.time.Duration
+import java.time.LocalDateTime
+
+
+fun main() {
+    defer3()
+}
+
+fun defer1() {
+    val justNow = Mono.just(LocalDateTime.now())
+    val defetNow = Mono.defer { Mono.just(LocalDateTime.now()) }
+
+    Thread.sleep(5_000)
+
+    justNow
+        .log()
+        .subscribe()
+
+    defetNow
+        .log()
+        .subscribe()
+
+    Thread.sleep(5_000)
+
+    justNow
+        .log()
+        .subscribe()
+
+    defetNow
+        .log()
+        .subscribe()
+}
+
+fun defer2() {
+    Mono
+        .just("Banana")
+        .delayElement(Duration.ofSeconds(5))
+        .switchIfEmpty(peekaboo())
+        .log()
+        .subscribe(::println)
+
+    Thread.sleep(6_000)
+}
+
+fun defer3() {
+    Mono
+        .just("Banana")
+        .delayElement(Duration.ofSeconds(5))
+        .switchIfEmpty(Mono.defer { peekaboo() })
+        .log()
+        .subscribe(::println)
+
+    Thread.sleep(6_000)
+}
+
+
+fun peekaboo(): Mono<String> {
+    println("peekaboo")
+    return Mono.just("Tomato")
+}
