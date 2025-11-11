2a5a583f61d790d533432c9bf7e7e6d7c0175a60
lkdcode <dlrlejr1@naver.com>
2025-11-11 23:34:03 +0900

    Chore(:init) message server

---
+++ server-practice/message-server/buildSrc/src/main/kotlin/DatabaseLibs.kt
@@ -0,0 +1,9 @@
+object DatabaseLibs {
+    const val PATH = "gradle/dep/database-dep.gradle"
+
+    const val JDBC = "org.springframework.boot:spring-boot-starter-jdbc"
+    const val R2DBC = "org.springframework.boot:spring-boot-starter-data-r2dbc"
+
+    const val POSTGRESQL_CONNECTOR = "org.postgresql:postgresql"
+    const val R2DBC_POSTGRESQL = "org.postgresql:r2dbc-postgresql"
+}
