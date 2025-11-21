6874ffa35e28dd5e8dd823e3036a0ce6c4381523
lkdcode <dlrlejr1@naver.com>
2025-11-21 21:00:00 +0900

    Feat(:auth) init

--- server-practice/message-server/message-adapter/adapter-post-webflux/src/main/kotlin/dev/lkdcode/infrastructure/websocket/handler/ChannelTypeHandler.kt
+++ server-practice/message-server/message-adapter/adapter-post-webflux/src/main/kotlin/dev/lkdcode/infrastructure/websocket/handler/ChannelTypeHandler.kt
@@ -1,8 +1,12 @@
 package dev.lkdcode.infrastructure.websocket.handler
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import dev.lkdcode.infrastructure.security.UserAuthentication
+import dev.lkdcode.infrastructure.websocket.enum.ChannelType
+import dev.lkdcode.infrastructure.websocket.request.SocketRequest
 import dev.lkdcode.infrastructure.websocket.session.SessionChannels
 import dev.lkdcode.infrastructure.websocket.session.SessionSinks
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.socket.WebSocketMessage
 import org.springframework.web.reactive.socket.WebSocketSession
@@ -20,6 +24,24 @@ class ChannelTypeHandler(
         return session
             .receive()
             .map(WebSocketMessage::getPayloadAsText)
+
+            .flatMap {
+                val message = objectMapper.readValue(it, SocketRequest::class.java)
+                val sessionId = session.id
+                val authentication = session.extractAuth()
+
+                authentication
+                    .flatMap { auth ->
+                        when (message.channelType) {
+                            ChannelType.DM -> Mono.just("DM")
+                            ChannelType.GM -> Mono.just("GM")
+                        }
+                    }
+                    .map { payload -> objectMapper.writeValueAsString(payload) }
+                    .flatMap { converted -> session.send(Mono.just(session.textMessage(converted))) }
+                    .onErrorResume(webSocketErrorHandler.handleError(session))
+            }
+
             .doFinally {
                 SessionSinks.remove(session.id)
                 SessionChannels.remove(session.id)
@@ -27,4 +49,10 @@ class ChannelTypeHandler(
             .then()
     }
 
+    private fun WebSocketSession.extractAuth() =
+        this.handshakeInfo
+            .principal
+            .cast(UsernamePasswordAuthenticationToken::class.java)
+            .map { it.principal }
+            .cast(UserAuthentication::class.java)
 }
