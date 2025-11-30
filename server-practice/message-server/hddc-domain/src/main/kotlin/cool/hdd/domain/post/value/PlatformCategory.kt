74edb1b45f37bc7af4a31f8e5dd01e297bc21737
lkdcode <dlrlejr1@naver.com>
2025-12-01 08:03:41 +0900

    Feat(:domain) add value

---
+++ server-practice/message-server/hddc-domain/src/main/kotlin/cool/hdd/domain/post/value/PlatformCategory.kt
@@ -0,0 +1,18 @@
+package cool.hdd.domain.post.value
+
+enum class PlatformCategory {
+    COUPANG,
+    NAVER_SHOPPING,
+    G_MARKET,
+    AUCTION,
+    ELVEN_STREET,
+    AMAZON,
+    LOTTE_ON,
+    SSG,
+    SSF,
+    HOMEPLUS,
+    ALI_EXPRESS,
+    MUSINSA,
+    INTERPARK,
+    HIMART,
+}
