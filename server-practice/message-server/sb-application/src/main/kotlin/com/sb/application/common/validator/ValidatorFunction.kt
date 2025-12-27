9bf524de400d7a343fcb5bc8f93da8154c8b316f
lkdcode <dlrlejr1@naver.com>
2025-12-27 11:25:42 +0900

    Feat(:util) validator function

---
+++ server-practice/message-server/sb-application/src/main/kotlin/com/sb/application/common/validator/ValidatorFunction.kt
@@ -0,0 +1,24 @@
+package com.sb.application.common.validator
+
+import com.sb.domain.exception.DomainErrorCode
+import com.sb.domain.exception.DomainValidationException
+import reactor.core.publisher.Mono
+
+
+fun throwIf(condition: Boolean, exception: Throwable) =
+    if (condition) throw exception else Unit
+
+fun throwUnless(condition: Boolean, exception: Throwable) =
+    if (!condition) throw exception else Unit
+
+fun throwIf(condition: Boolean, domainErrorCode: DomainErrorCode) =
+    if (condition) throw DomainValidationException(domainErrorCode) else Unit
+
+fun throwUnless(condition: Boolean, domainErrorCode: DomainErrorCode) =
+    if (!condition) throw DomainValidationException(domainErrorCode) else Unit
+
+fun monoErrorIf(condition: Boolean, domainErrorCode: DomainErrorCode): Mono<Unit> =
+    if (condition) Mono.error(DomainValidationException(domainErrorCode)) else Mono.empty()
+
+fun monoErrorUnless(condition: Boolean, domainErrorCode: DomainErrorCode): Mono<Unit> =
+    if (!condition) Mono.error(DomainValidationException(domainErrorCode)) else Mono.empty()
