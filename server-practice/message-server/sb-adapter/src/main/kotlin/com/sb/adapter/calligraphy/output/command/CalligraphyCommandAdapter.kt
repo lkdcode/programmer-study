e8d1e5271ad0d6bcbce287dbbd107ee283257c13
lkdcode <dlrlejr1@naver.com>
2025-12-27 10:36:14 +0900

    Feat(:calligraphy) payment adapter, command adapter

---
+++ server-practice/message-server/sb-adapter/src/main/kotlin/com/sb/adapter/calligraphy/output/command/CalligraphyCommandAdapter.kt
@@ -0,0 +1,29 @@
+package com.sb.adapter.calligraphy.output.command
+
+import com.sb.adapter.calligraphy.output.infrastructure.r2dbc.mapper.toAggregate
+import com.sb.adapter.calligraphy.output.infrastructure.r2dbc.mapper.toR2dbcEntity
+import com.sb.adapter.calligraphy.output.infrastructure.r2dbc.repository.CalligraphyR2dbcRepository
+import com.sb.application.calligraphy.ports.output.command.CalligraphyCommandPort
+import com.sb.domain.calligraphy.aggregate.CalligraphyAggregate
+import com.sb.domain.calligraphy.command.CreateCalligraphyCommand
+import kotlinx.coroutines.reactive.awaitSingle
+import org.springframework.stereotype.Component
+
+
+@Component
+class CalligraphyCommandAdapter(
+    private val repository: CalligraphyR2dbcRepository
+) : CalligraphyCommandPort {
+
+    override suspend fun save(aggregate: CalligraphyAggregate): CalligraphyAggregate =
+        repository
+            .save(aggregate.toR2dbcEntity())
+            .awaitSingle()
+            .toAggregate()
+
+    override suspend fun save(command: CreateCalligraphyCommand): CalligraphyAggregate =
+        repository
+            .save(command.toR2dbcEntity())
+            .awaitSingle()
+            .toAggregate()
+}
