8bce3554a01ee7e9360102a0db72c3cf0aacc4dd
lkdcode <dlrlejr1@naver.com>
2025-12-28 21:32:05 +0900

    Feat(:like) add entity

--- server-practice/message-server/sb-domain/src/main/kotlin/com/sb/domain/like/repository/CalligraphyLikeQueryRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.sb.domain.like.repository
-
-import com.sb.domain.calligraphy.entity.Calligraphy
-import com.sb.domain.calligraphy.value.User
-
-interface CalligraphyLikeQueryRepository {
-    suspend fun existsBy(calligraphyId: Calligraphy.CalligraphyId, user: User): Boolean
-    suspend fun countBy(calligraphyId: Calligraphy.CalligraphyId): Long
-}
