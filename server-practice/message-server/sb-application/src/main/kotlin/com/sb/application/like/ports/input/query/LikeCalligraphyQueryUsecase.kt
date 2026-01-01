eddc400a6f890a681b000c478984f422eeb00552
lkdcode <dlrlejr1@naver.com>
2026-01-01 17:48:02 +0900

    Feat(:like) query usecase

--- server-practice/message-server/sb-application/src/main/kotlin/com/sb/application/like/ports/input/query/LikeCalligraphyQueryUsecase.kt
+++ server-practice/message-server/sb-application/src/main/kotlin/com/sb/application/like/ports/input/query/LikeCalligraphyQueryUsecase.kt
@@ -1,8 +1,14 @@
 package com.sb.application.like.ports.input.query
 
-import com.sb.application.calligraphy.dto.GetLikedCalligraphyQuery
-import com.sb.domain.calligraphy.model.ShowcaseCalligraphy
+import com.sb.application.calligraphy.dto.query.GetLikedCalligraphiesCursorQuery
+import com.sb.application.calligraphy.dto.query.GetLikedCalligraphiesPageQuery
+import com.sb.application.calligraphy.dto.query.GetLikedCalligraphiesSliceQuery
+import com.sb.application.calligraphy.dto.query.ShowcaseCalligraphyCursor
+import com.sb.application.calligraphy.dto.query.ShowcaseCalligraphyPage
+import com.sb.application.calligraphy.dto.query.ShowcaseCalligraphySlice
 
 interface LikeCalligraphyQueryUsecase {
-    suspend fun fetchMyLikedCalligraphies(query: GetLikedCalligraphyQuery): List<ShowcaseCalligraphy>
+    suspend fun fetchMyLikedCalligraphies(query: GetLikedCalligraphiesPageQuery): ShowcaseCalligraphyPage
+    suspend fun fetchMyLikedCalligraphies(query: GetLikedCalligraphiesSliceQuery): ShowcaseCalligraphySlice
+    suspend fun fetchMyLikedCalligraphies(query: GetLikedCalligraphiesCursorQuery): ShowcaseCalligraphyCursor
 }
