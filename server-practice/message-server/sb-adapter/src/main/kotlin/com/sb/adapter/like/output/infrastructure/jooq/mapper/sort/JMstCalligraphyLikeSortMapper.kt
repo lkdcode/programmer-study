a80140881572c9d7bc5e7faad520dbc7d06fd9ef
lkdcode <dlrlejr1@naver.com>
2026-01-02 19:03:25 +0900

    Feat(:jooq) add sort option

--- server-practice/message-server/sb-adapter/src/main/kotlin/com/sb/adapter/like/output/infrastructure/jooq/mapper/sort/JMstCalligraphyLikeSortMapper.kt
+++ server-practice/message-server/sb-adapter/src/main/kotlin/com/sb/adapter/like/output/infrastructure/jooq/mapper/sort/JMstCalligraphyLikeSortMapper.kt
@@ -1,28 +1,23 @@
 package com.sb.adapter.like.output.infrastructure.jooq.mapper.sort
 
-import com.sb.application.common.input.query.sort.SortDirection
 import com.sb.application.common.input.query.sort.SortOptionList
+import com.sb.framework.jooq.mapper.sort.sortOption
 import com.sb.jooq.tables.references.JMST_CALLIGRAPHY_LIKE
 import org.jooq.SortField
 
+private val DEFAULT_SORT = JMST_CALLIGRAPHY_LIKE.CREATED_AT.desc()
 
-fun SortOptionList?.toCalligraphyLikeSortField(): List<SortField<*>>? =
+private val AVAILABLE_PROPERTY = mapOf(
+    "id" to JMST_CALLIGRAPHY_LIKE.ID,
+    "createdAt" to JMST_CALLIGRAPHY_LIKE.CREATED_AT,
+)
+
+fun SortOptionList?.toCalligraphyLikeSortField(): List<SortField<*>> =
     this
         ?.list
-        ?.mapNotNull {
-            when (it.property) {
-                "id" -> {
-                    if (it.direction == SortDirection.ASC) JMST_CALLIGRAPHY_LIKE.ID.asc()
-                    else JMST_CALLIGRAPHY_LIKE.ID.desc()
-                }
-
-                "createdAt" -> {
-                    if (it.direction == SortDirection.ASC) JMST_CALLIGRAPHY_LIKE.CREATED_AT.asc()
-                    else JMST_CALLIGRAPHY_LIKE.CREATED_AT.desc()
-
-                }
-
-                else -> null
-            }
+        .orEmpty()
+        .mapNotNull {
+            AVAILABLE_PROPERTY[it.property]
+                ?.sortOption(it.direction)
         }
-        ?.toList()
+        .ifEmpty { listOf(DEFAULT_SORT) }
