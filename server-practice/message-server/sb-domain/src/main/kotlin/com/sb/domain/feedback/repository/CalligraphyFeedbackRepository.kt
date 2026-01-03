e78e6e9b34b630ecc0e6ac562a7073b3c9a3a7be
lkdcode <dlrlejr1@naver.com>
2026-01-03 23:03:36 +0900

    Refactor(:feedback)
    - remove repository
    - author -> userId

--- server-practice/message-server/sb-domain/src/main/kotlin/com/sb/domain/feedback/repository/CalligraphyFeedbackRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.sb.domain.feedback.repository
-
-import com.sb.domain.feedback.aggregate.CalligraphyFeedbackAggregate
-
-interface CalligraphyFeedbackRepository {
-    suspend fun save(aggregate: CalligraphyFeedbackAggregate): CalligraphyFeedbackAggregate
-}
