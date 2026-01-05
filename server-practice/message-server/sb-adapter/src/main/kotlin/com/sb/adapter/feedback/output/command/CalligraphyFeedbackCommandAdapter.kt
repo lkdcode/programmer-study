5d00bb5a95b6dbd9a36471626d0d38d72e70432f
lkdcode <dlrlejr1@naver.com>
2026-01-05 22:34:02 +0900

    Feat(:feedback) adapter
    - delete
    - validation
    - load extensions

--- server-practice/message-server/sb-adapter/src/main/kotlin/com/sb/adapter/feedback/output/command/CalligraphyFeedbackCommandAdapter.kt
+++ server-practice/message-server/sb-adapter/src/main/kotlin/com/sb/adapter/feedback/output/command/CalligraphyFeedbackCommandAdapter.kt
@@ -3,10 +3,13 @@ package com.sb.adapter.feedback.output.command
 import com.sb.adapter.feedback.output.infrastructure.r2dbc.mapper.entity.toAggregate
 import com.sb.adapter.feedback.output.infrastructure.r2dbc.mapper.entity.toEntity
 import com.sb.adapter.feedback.output.infrastructure.r2dbc.repository.CalligraphyFeedbackR2dbcRepository
+import com.sb.adapter.feedback.output.infrastructure.r2dbc.repository.loadById
 import com.sb.application.feedback.dto.command.CreateFeedbackCommand
 import com.sb.application.feedback.ports.output.command.CalligraphyFeedbackCommandPort
 import com.sb.domain.feedback.aggregate.CalligraphyFeedbackAggregate
+import com.sb.domain.feedback.entity.CalligraphyFeedback
 import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.stereotype.Component
 
 
@@ -20,4 +23,15 @@ class CalligraphyFeedbackCommandAdapter(
             .save(command.toEntity())
             .map { it.toAggregate() }
             .awaitSingle()
+
+    override suspend fun delete(id: CalligraphyFeedback.CalligraphyFeedbackId) {
+        repository
+            .loadById(id)
+            .flatMap {
+                it.delete()
+                repository.save(it)
+            }
+            .then()
+            .awaitSingleOrNull()
+    }
 }
