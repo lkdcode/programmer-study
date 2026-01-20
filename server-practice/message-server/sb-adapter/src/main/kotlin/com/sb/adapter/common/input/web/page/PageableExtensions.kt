2788516911d55cc93660396c426a8eba630809a5
lkdcode <dlrlejr1@naver.com>
2026-01-20 20:31:34 +0900

    Feat(:calligraphy) query

--- server-practice/message-server/sb-adapter/src/main/kotlin/com/sb/adapter/common/input/web/page/PageableExtensions.kt
+++ server-practice/message-server/sb-adapter/src/main/kotlin/com/sb/adapter/common/input/web/page/PageableExtensions.kt
@@ -4,15 +4,20 @@ import com.sb.application.common.input.query.sort.SortDirection
 import com.sb.application.common.input.query.sort.SortOption
 import com.sb.application.common.input.query.sort.SortOptionList
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 
-fun Pageable.toSortOptionList(): SortOptionList? =
-    this.sort
-        .filter { it.property.isNotBlank() }
-        .map { order ->
-            SortOption(
-                order.property,
-                if (order.direction.name == SortDirection.DESC.name) SortDirection.DESC else SortDirection.ASC
-            )
-        }
-        .toList()
-        .let { SortOptionList(it) }
+fun Pageable.toSortOptionList(): SortOptionList {
+    val options = sort.map { order ->
+        SortOption(
+            property = order.property,
+            direction = order.direction.toSortDirection()
+        )
+    }.toList()
+
+    return SortOptionList(options)
+}
+
+private fun Sort.Direction.toSortDirection(): SortDirection = when (this) {
+    Sort.Direction.ASC -> SortDirection.ASC
+    Sort.Direction.DESC -> SortDirection.DESC
+}
