ff146b3e4ad5d3a487601ef07b1b3ba3438d82bf
lkdcode <dlrlejr1@naver.com>
2026-01-23 23:29:59 +0900

    Init(:buildSrc) reactive cache

---
+++ api-practice/reactive-cache/buildSrc/src/main/kotlin/SpringBootLibs.kt
@@ -0,0 +1,15 @@
+object SpringBootLibs {
+    const val PATH = "gradle/spring-boot-dep.gradle"
+
+    const val SECURITY = "org.springframework.boot:spring-boot-starter-security"
+
+    const val WEB = "org.springframework.boot:spring-boot-starter-web"
+    const val DATA_JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
+    const val VALIDATION = "org.springframework.boot:spring-boot-starter-validation"
+    const val TEST = "org.springframework.boot:spring-boot-starter-test"
+
+    const val SECURITY_TEST = "org.springframework.security:spring-security-test"
+    const val QUARTZ = "org.springframework.boot:spring-boot-starter-quartz"
+
+    const val MONGO_DB = "org.springframework.boot:spring-boot-starter-data-mongodb"
+}
---
+++ api-practice/reactive-cache/buildSrc/src/main/kotlin/SpringWebFluxLibs.kt
@@ -0,0 +1,23 @@
+object SpringWebFluxLibs {
+    const val PATH = "gradle/spring-webflux-dep.gradle"
+
+    const val WEB_FLUX = "org.springframework.boot:spring-boot-starter-webflux"
+    const val SECURITY = "org.springframework.boot:spring-boot-starter-security"
+    const val R2DBC = "org.springframework.boot:spring-boot-starter-data-r2dbc"
+
+    const val MAIL = "org.springframework.boot:spring-boot-starter-mail"
+    const val VALIDATION = "org.springframework.boot:spring-boot-starter-validation"
+    const val AOP = "org.springframework.boot:spring-boot-starter-aop"
+    const val REFLECTION = "org.reflections:reflections:0.10.2"
+
+    const val REDIS_REACTIVE = "org.springframework.boot:spring-boot-starter-data-redis-reactive"
+    const val MONGODB_REACTIVE = "org.springframework.boot:spring-boot-starter-data-mongodb-reactive"
+    const val TEST = "org.springframework.boot:spring-boot-starter-test"
+    const val JUG = "com.fasterxml.uuid:java-uuid-generator:5.1.1"
+
+    const val REACTOR_TEST = "io.projectreactor:reactor-test"
+
+    const val REACTIVE_STREAMS = "org.reactivestreams:reactive-streams:1.0.4"
+    const val REACTOR_CORE = "io.projectreactor:reactor-core:3.6.4"
+    const val NETTY_WITH_MAC_OS_AARCH = "io.netty:netty-resolver-dns-native-macos:4.2.1.Final:osx-aarch_64"
+}
