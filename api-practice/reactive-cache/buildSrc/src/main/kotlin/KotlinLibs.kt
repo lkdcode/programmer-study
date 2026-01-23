a5ae2ba05f3ce7fa892a7764b197b34abfd23d95
lkdcode <dlrlejr1@naver.com>
2026-01-23 23:29:59 +0900

    Init(:buildSrc) reactive cache

---
+++ api-practice/reactive-cache/buildSrc/src/main/kotlin/KotlinLibs.kt
@@ -0,0 +1,10 @@
+object KotlinLibs {
+    const val PATH = "gradle/kotlin-dep.gradle"
+
+    const val JACKSON_MODULE_KOTLIN = "com.fasterxml.jackson.module:jackson-module-kotlin"
+    const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect"
+    const val JSR305_STRICT_MODE = "-Xjsr305=strict"
+    const val COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2"
+    const val COROUTINES_REACTOR = "org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.1"
+    const val REACTOR_EXTENSIONS = "io.projectreactor.kotlin:reactor-kotlin-extensions"
+}
