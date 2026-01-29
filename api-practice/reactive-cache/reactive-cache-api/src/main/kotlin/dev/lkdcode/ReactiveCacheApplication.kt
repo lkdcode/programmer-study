91dae745a6798d59dd34235d02d941418f7af23d
lkdcode <dlrlejr1@naver.com>
2026-01-29 23:04:13 +0900

    Chore(:gradle) add BlockHound

--- api-practice/reactive-cache/reactive-cache-api/src/main/kotlin/dev/lkdcode/ReactiveCacheApplication.kt
+++ api-practice/reactive-cache/reactive-cache-api/src/main/kotlin/dev/lkdcode/ReactiveCacheApplication.kt
@@ -2,10 +2,12 @@ package dev.lkdcode
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.blockhound.BlockHound
 
 @SpringBootApplication
 class ReactiveCacheApplication
 
 fun main(args: Array<String>) {
+    BlockHound.install()
     runApplication<ReactiveCacheApplication>(*args)
 }
