5247c381717349f2d5cec756fec27d5fa7ad2b65
lkdcode <dlrlejr1@naver.com>
2026-02-05 22:58:42 +0900

    Feat(:cache) soft ttl, pub/sub

--- api-practice/reactive-cache/reactive-cache-api/src/main/kotlin/dev/lkdcode/cache/service/CacheService.kt
+++ api-practice/reactive-cache/reactive-cache-api/src/main/kotlin/dev/lkdcode/cache/service/CacheService.kt
@@ -1,5 +1,6 @@
 package dev.lkdcode.cache.service
 
+import dev.lkdcode.cache.model.CacheModel
 import reactor.core.publisher.Mono
 import java.time.Duration
 
@@ -11,4 +12,10 @@ interface CacheService {
 
     fun tryLock(key: String, ttl: Duration = Duration.ofSeconds(10L)): Mono<Boolean>
     fun unlock(key: String): Mono<Void>
+
+    fun publishCacheReady(key: String): Mono<Long>
+    fun subscribeCacheReady(key: String, timeout: Duration = Duration.ofSeconds(5L)): Mono<String>
+
+    fun getValueWithTimestamp(key: String): Mono<CacheModel>
+    fun saveWithTimestamp(key: String, value: Any, ttl: Duration): Mono<Void>
 }
--- api-practice/reactive-cache/reactive-cache-api/src/main/kotlin/dev/lkdcode/cache/service/ReactiveRedisCacheService.kt
+++ api-practice/reactive-cache/reactive-cache-api/src/main/kotlin/dev/lkdcode/cache/service/ReactiveRedisCacheService.kt
@@ -1,14 +1,19 @@
 package dev.lkdcode.cache.service
 
+import dev.lkdcode.cache.model.CacheModel
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.core.ReactiveRedisOperations
 import org.springframework.data.redis.core.ScanOptions
+import org.springframework.data.redis.listener.ChannelTopic
+import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import java.time.Duration
 
 @Component
 class ReactiveRedisCacheService(
-    private val redisOps: ReactiveRedisOperations<String, Any>
+    private val redisOps: ReactiveRedisOperations<String, Any>,
+    private val connectionFactory: ReactiveRedisConnectionFactory
 ) : CacheService {
 
     override fun getValue(key: String): Mono<Any> =
@@ -16,6 +21,17 @@ class ReactiveRedisCacheService(
 
     override fun save(key: String, value: Any, ttl: Duration): Mono<Void> =
         redisOps.opsForValue().set(key, value, ttl).then()
+
+    override fun getValueWithTimestamp(key: String): Mono<CacheModel> =
+        redisOps.opsForValue().get(softTtlKey(key))
+            .filter { it is CacheModel }
+            .cast(CacheModel::class.java)
+
+    override fun saveWithTimestamp(key: String, value: Any, ttl: Duration): Mono<Void> {
+        val cachedValue = CacheModel(value = value)
+
+        return redisOps.opsForValue().set(softTtlKey(key), cachedValue, ttl).then()
+    }
 
     override fun delete(key: String): Mono<Void> =
         redisOps.opsForValue().delete(key).then()
@@ -35,9 +51,28 @@ class ReactiveRedisCacheService(
     override fun unlock(key: String): Mono<Void> =
         redisOps.opsForValue().delete(lockKey(key)).then()
 
+    override fun publishCacheReady(key: String): Mono<Long> =
+        redisOps.convertAndSend(cacheReadyChannel(key), CACHE_READY_MESSAGE)
+
+    override fun subscribeCacheReady(key: String, timeout: Duration): Mono<String> {
+        val container = ReactiveRedisMessageListenerContainer(connectionFactory)
+        val topic = ChannelTopic.of(cacheReadyChannel(key))
+
+        return container.receive(topic)
+            .next()
+            .map { it.message.toString() }
+            .timeout(timeout)
+            .doFinally { container.destroyLater().subscribe() }
+    }
+
     private fun lockKey(key: String): String = "$LOCK_VALUE:$key"
+    private fun cacheReadyChannel(key: String): String = "$CHANNEL_PREFIX:$key"
+    private fun softTtlKey(key: String): String = "$SOFT_TTL_PREFIX:$key"
 
     companion object {
         private const val LOCK_VALUE = "LOCKED"
+        private const val CHANNEL_PREFIX = "CACHE:READY"
+        private const val CACHE_READY_MESSAGE = "READY"
+        private const val SOFT_TTL_PREFIX = "SOFT_TTL"
     }
 }
