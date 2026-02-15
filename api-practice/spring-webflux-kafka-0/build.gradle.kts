42692df4a5c1e877df645d849e24c0e97e81f7f4
lkdcode <dlrlejr1@naver.com>
2026-02-15 23:18:48 +0900

    Chore(:init) kafka

---
+++ api-practice/spring-webflux-kafka-0/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+
+    id(SpringBoot.ID) version SpringBoot.VERSION apply false
+    id(SpringBoot.DEPENDENCY_MANAGEMENT) version SpringBoot.DEPENDENCY_MANAGEMENT_VERSION apply false
+
+    kotlin(Kotlin.WITH_JVM) version Kotlin.KOTLIN_VERSION apply false
+    kotlin(Kotlin.WITH_SPRING) version Kotlin.KOTLIN_VERSION apply false
+    kotlin(Kotlin.KAPT) version Kotlin.KOTLIN_VERSION apply false
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(Versioning.JAVA)
+    }
+}
+
+allprojects {
+    group = Versioning.GROUP
+    version = Versioning.VERSION
+
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
---
+++ api-practice/spring-webflux-kafka-0/buildSrc/src/main/kotlin/SpringWebFlux.kt
@@ -0,0 +1,9 @@
+object SpringWebFlux {
+    const val PATH = "gradle/dep/spring-webflux-dep.gradle"
+
+    const val WEB_FLUX = "org.springframework.boot:spring-boot-starter-webflux"
+    const val R2DBC = "org.springframework.boot:spring-boot-starter-data-r2dbc"
+
+    const val TEST = "org.springframework.boot:spring-boot-starter-test"
+    const val REACTOR_TEST = "io.projectreactor:reactor-test"
+}
