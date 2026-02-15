1c4f56db7d0bfa2e8bce3cca90659ee06e2e313d
lkdcode <dlrlejr1@naver.com>
2026-02-15 23:34:00 +0900

    Feat(:kafka) producer config

--- api-practice/spring-webflux-kafka-0/kafka-producer-0/src/main/kotlin/dev/lkdcode/kafka/framework/kafka/config/KafkaProducerConfig.kt
+++ api-practice/spring-webflux-kafka-0/kafka-producer-0/src/main/kotlin/dev/lkdcode/kafka/framework/kafka/config/KafkaProducerConfig.kt
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import reactor.kafka.sender.SenderOptions
 
@@ -14,18 +15,47 @@ class KafkaProducerConfig(
     private val bootstrapServers: String,
 ) {
 
+    private fun baseProps(): Map<String, Any> = mapOf(
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+    )
+
     @Bean
-    fun reactiveKafkaProducerTemplate(): ReactiveKafkaProducerTemplate<String, String> {
-        val props = mapOf(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+    @Primary
+    fun reliableProducerTemplate(): ReactiveKafkaProducerTemplate<String, String> {
+        val props = baseProps() + mapOf(
             ProducerConfig.ACKS_CONFIG to "all",
-            ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG to true
+            ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG to true,
+            ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG to 30_000,
+            ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG to 10_000,
         )
 
-        val senderOptions = SenderOptions.create<String, String>(props)
-        return ReactiveKafkaProducerTemplate(senderOptions)
+        return ReactiveKafkaProducerTemplate(SenderOptions.create(props))
+    }
 
+    @Bean
+    fun highThroughputProducerTemplate(): ReactiveKafkaProducerTemplate<String, String> {
+        val props = baseProps() + mapOf(
+            ProducerConfig.ACKS_CONFIG to "1",
+            ProducerConfig.LINGER_MS_CONFIG to 20,
+            ProducerConfig.BATCH_SIZE_CONFIG to 65_536,
+            ProducerConfig.COMPRESSION_TYPE_CONFIG to "snappy",
+            ProducerConfig.BUFFER_MEMORY_CONFIG to 33_554_432L,
+        )
+
+        return ReactiveKafkaProducerTemplate(SenderOptions.create(props))
+    }
+
+    @Bean
+    fun fireAndForgetProducerTemplate(): ReactiveKafkaProducerTemplate<String, String> {
+        val props = baseProps() + mapOf(
+            ProducerConfig.ACKS_CONFIG to "0",
+            ProducerConfig.RETRIES_CONFIG to 0,
+            ProducerConfig.LINGER_MS_CONFIG to 5,
+            ProducerConfig.BATCH_SIZE_CONFIG to 32_768,
+        )
+
+        return ReactiveKafkaProducerTemplate(SenderOptions.create(props))
     }
 }
