fac1daadd94258563deb686c3eb80aabf642fb43
lkdcode <dlrlejr1@naver.com>
2026-02-24 07:55:35 +0900

    Feat(:gradle) module

--- api-practice/spring-webflux-kafka-0/settings.gradle.kts
+++ api-practice/spring-webflux-kafka-0/settings.gradle.kts
@@ -2,4 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "spring-webflux-kafka-0"
+include("kafka-common")
 include("kafka-producer-0")
+include("kafka-producer-1")
+include("kafka-consumer-0")
