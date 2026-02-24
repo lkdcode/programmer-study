ca2a88c8fdf0ee0c4e5ec87915d2a1f13a9d9851
lkdcode <dlrlejr1@naver.com>
2026-02-25 08:01:56 +0900

    Feat(:common) 일급 컬렉션과 convert

--- api-practice/spring-webflux-kafka-0/kafka-common/src/main/kotlin/dev/lkdcode/kafka/domains/adapter/output/kafka/mapper/TomatoDtoMapper.kt
+++ api-practice/spring-webflux-kafka-0/kafka-common/src/main/kotlin/dev/lkdcode/kafka/domains/adapter/output/kafka/mapper/TomatoDtoMapper.kt
@@ -1,7 +1,11 @@
 package dev.lkdcode.kafka.domains.adapter.output.kafka.mapper
 
 import dev.lkdcode.kafka.domains.application.dto.TomatoDto
+import dev.lkdcode.kafka.domains.application.dto.TomatoDtoList
 import reactor.kafka.sender.SenderResult
+
+fun List<SenderResult<Void>>.convert(): TomatoDtoList =
+    TomatoDtoList(this.map { it.convert() }.toList())
 
 fun SenderResult<Void>.convert(): TomatoDto =
     TomatoDto(
--- api-practice/spring-webflux-kafka-0/kafka-common/src/main/kotlin/dev/lkdcode/kafka/domains/application/dto/TomatoDto.kt
+++ api-practice/spring-webflux-kafka-0/kafka-common/src/main/kotlin/dev/lkdcode/kafka/domains/application/dto/TomatoDto.kt
@@ -1,5 +1,9 @@
 package dev.lkdcode.kafka.domains.application.dto
 
+data class TomatoDtoList(
+    val items: List<TomatoDto>
+)
+
 data class TomatoDto(
     val offset: Long,
     val partition: Int,
